7999e2b0d4425051ad5827a1df89b5301f738fbc
chim_98 <chavividusha@gmail.com>
2021-03-19 15:11:30 +0530

    toast msg added. color changes added to match the theme colors.

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Stage} from 'react-konva';
 import {Button, Col, Image, Row} from "react-bootstrap";
-import {FaFileDownload, FaTrash,FaComment} from 'react-icons/fa';
+import {FaFileDownload, FaTrash, FaComment} from 'react-icons/fa';
 import phone from "../../assets/images/hpne.png";
 import {ElementFactory} from "../../elementFactory/elementFactory";
 import {connect} from "react-redux";
@@ -11,15 +11,29 @@ import {confirmAlert} from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css';
 import ReactStars from "react-rating-stars-component";
 import {saveReviews} from "../../actions/DataStoreManager";
+import ReactSnackBar from "react-js-snackbar";
 
 function Canvas({fetchElements, element}) {
 
-    const [rating, setRating] = useState(2);
+    const [rating, setRating] = useState(0);
     const [comment, setComment] = useState();
+    const [showToast, setShowToast] = useState(false);
+    const [toastShowing, setToastShowing] = useState(false);
+
     var uniqid = require('uniqid');
 
     const ratingChanged = (newRating) => {
         setRating(newRating);
+    };
+
+    const toast = () => {
+        if (toastShowing) return;
+            setShowToast(true);
+            setToastShowing(true);
+            setTimeout(() => {
+                setShowToast(false);
+                setToastShowing(false)
+            }, 2000);
     };
 
 
@@ -34,7 +48,7 @@ function Canvas({fetchElements, element}) {
                         <Image src={phone} fluid style={{position: "absolute"}} width={200} height={400}/>
                         <Stage width={200} height={400} style={{padding: 20}}>
 
-                            {element  &&
+                            {element &&
                             <ElementFactory element={element}/>
                             }
 
@@ -48,15 +62,23 @@ function Canvas({fetchElements, element}) {
                         <span style={{textAlign: "center", marginBottom: 30}}>Please use the below button to download the generated sketch UI design as a PDF.</span>
                         <Pdf targetRef={ref} filename="div-blue.pdf" scale={2} x={50} y={20}>
                             {({toPdf}) => (
-                                <Button variant="outline-light" style={{borderColor: "#EC7A23", width: 130, marginBottom: 40}}
-                                onClick={()=>{
-                                    saveReviews({collectionName:"UISketcherUserReviews",documentID:uniqid(),ratingValue:rating,userComment:comment});
-                                    toPdf()
-                                }}>
-                            <FaFileDownload size={15} color={"#EC7A23"}/>
-                            <span style={{color: '#34495e', margin: 5}}>Download</span>
-                        </Button>
-                                )}
+                                <Button variant="outline-light"
+                                        style={{borderColor: "#EC7A23", width: 130, marginBottom: 40}}
+                                        onClick={() => {
+                                            saveReviews({
+                                                collectionName: "UISketcherUserReviews",
+                                                documentID: uniqid(),
+                                                ratingValue: rating,
+                                                userComment: comment
+                                            });
+                                            toPdf();
+                                            setRating(0);
+                                            setComment("");
+                                        }}>
+                                    <FaFileDownload size={15} color={"#EC7A23"}/>
+                                    <span style={{color: '#34495e', margin: 5}}>Download</span>
+                                </Button>
+                            )}
                         </Pdf>
 
                     </Row>
@@ -67,44 +89,48 @@ function Canvas({fetchElements, element}) {
                                     confirmAlert({
                                         customUI: ({onClose}) => {
                                             return (
-
-                                                        <div className='custom-ui'>
-                                                            <h3>Please rate you sketch design.</h3>
-                                                            <ReactStars
-                                                                count={5}
-                                                                onChange={ratingChanged}
-                                                                size={40}
-                                                                activeColor="#ffd700"
-                                                            />
-                                                            <br/>
-                                                            <textarea
-                                                                rows={4}
-                                                                   style={{borderRadius: 20, flex: 1,width:"100%"}}
-                                                                   value={comment}
-                                                                   onChange={event => setComment(event.target.value)}
-                                                            />
-                                                            <br/>
-                                                            <br/>
-                                                            <button style={{
-                                                                borderRadius: 10,
-                                                                padding: 5,
-                                                                backgroundColor: "grey",
-                                                                color: 'white',
-                                                                marginRight: 15
-                                                            }} onClick={onClose}>Cancel
-                                                            </button>
-                                                            <button style={{
-                                                                borderRadius: 10,
-                                                                padding: 5,
-                                                                backgroundColor: "orange",
-                                                                color: 'white'
-                                                            }} onClick={() => {
-
-                                                                onClose()
-                                                            }}>
-                                                                Submit
-                                                            </button>
-                                                        </div>
+                                                <div className='custom-ui'>
+                                                    <h3>Please rate you sketch design.</h3>
+                                                    <ReactStars
+                                                        count={5}
+                                                        onChange={ratingChanged}
+                                                        size={40}
+                                                        activeColor="#EC7A23"
+                                                    />
+                                                    <br/>
+                                                    <textarea
+                                                        rows={4}
+                                                        style={{borderRadius: 20, flex: 1, width: "100%",paddingTop:10,paddingLeft:10}}
+                                                        value={comment}
+                                                        onChange={event => setComment(event.target.value)}
+                                                    />
+                                                    <br/>
+                                                    <br/>
+                                                    <button style={{
+                                                        borderRadius: 10,
+                                                        padding: 5,
+                                                        paddingLeft:10,
+                                                        paddingRight :10,
+                                                        backgroundColor: "#282F33",
+                                                        color: 'white',
+                                                        marginRight: 15
+                                                    }} onClick={onClose}>Cancel
+                                                    </button>
+                                                    <button style={{
+                                                        borderRadius: 10,
+                                                        padding: 5,
+                                                        paddingLeft:10,
+                                                        paddingRight :10,
+                                                        borderColor: "white",
+                                                        backgroundColor: "#EC7A23",
+                                                        color: 'white'
+                                                    }} onClick={() => {
+                                                            onClose();
+                                                            toast();
+                                                    }}>
+                                                        Submit
+                                                    </button>
+                                                </div>
                                             );
                                         }
                                     });
@@ -118,7 +144,7 @@ function Canvas({fetchElements, element}) {
                     <Row className={"justify-content-center"}>
                         <span style={{textAlign: "center", marginBottom: 30}}>Please use the below button to clear the canvas.</span>
                         <Button variant="outline-light" style={{borderColor: "#EC7A23", width: 130}}
-                        onClick={() => refreshPage()}>
+                                onClick={() => refreshPage()}>
                             <FaTrash size={15} color={"#EC7A23"}/>
                             <span style={{color: '#34495e', margin: 5}}>Reset</span>
                         </Button>
@@ -127,6 +153,9 @@ function Canvas({fetchElements, element}) {
                     </Row>
                 </Col>
             </Row>
+            <ReactSnackBar Show={showToast} Icon={<FaComment size={20} color={"#EC7A23"}/>}>
+                {rating !== 0 ? "User review saved. Thank you !" : "Failed attempt ! Please add a rating"}
+            </ReactSnackBar>
         </div>
 
     )
--- src/components/inputForm/inputForm.js
+++ src/components/inputForm/inputForm.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import {Button, Card, Form, InputGroup} from "react-bootstrap";
-import {FaMicrophone, FaStop, FaResolving} from 'react-icons/fa';
+import {Button, Card, Form, InputGroup,} from "react-bootstrap";
+import {FaMicrophone, FaResolving,FaPushed} from 'react-icons/fa';
 import {SiMicrogenetics} from 'react-icons/si';
 import SpeechRecognition, {useSpeechRecognition} from 'react-speech-recognition'
 import {fetchElements} from "../../actions";
@@ -23,7 +23,7 @@ function InputForm({dispatch, fetchElements, isLoading}) {
     return (
         <>
             {isLoading ?
-                <div>Loading ...</div> :
+                <div><span style={{color:"#34495e"}}>Loading ... Please wait.</span></div> :
                 <>
                     <Card className={"text-center"} style={{borderRadius: 60}}>
                         <Card.Body>
@@ -35,24 +35,24 @@ function InputForm({dispatch, fetchElements, isLoading}) {
                                     </Form.Control>
                                     <InputGroup.Append>
                                         <Button onClick={SpeechRecognition.startListening} variant="light"
-                                                style={{borderRadius: 20, margin: 10}}><FaMicrophone color={'red'}
+                                                style={{borderRadius: 20, margin: 10}}><FaMicrophone color={'F19B55'}
                                                                                                      size={35}/>
                                             <div>Record</div>
                                         </Button>
                                         <Button onClick={SpeechRecognition.stopListening} variant="light"
-                                                style={{borderRadius: 20, margin: 10}}><FaStop color={'grey'}
+                                                style={{borderRadius: 20, margin: 10}}><FaPushed color={'#BEBBB7'}
                                                                                                size={35}/>
                                             <div>Stop</div>
                                         </Button>
                                         <Button onClick={resetTranscript} variant="light"
-                                                style={{borderRadius: 20, margin: 10}}><FaResolving color={'green'}
+                                                style={{borderRadius: 20, margin: 10}}><FaResolving color={'#282F33'}
                                                                                                     size={35}/>
                                             <div>Reset</div>
                                         </Button>
 
                                     </InputGroup.Append>
                                 </InputGroup>
-                                <Button variant="success" style={{borderRadius: 50}}
+                                <Button variant="success" style={{borderRadius: 50,backgroundColor:"#EC7A23",borderColor:"#EC7A23"}}
                                         onClick={() => fetchElements(input)}>
                                     <SiMicrogenetics color={'white'} size={35} style={{marginLeft: 5, marginRight: 8}}/>
                                     Generate
